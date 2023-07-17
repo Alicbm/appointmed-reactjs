3bf3f6d299621096e35a7dc75250036e2745f59c
alicbm <abarandica1234@gmail.com>
2023-07-17 09:16:27 -0500

    doctors route ready with the mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, HashRouter, Routes } from 'react-router-dom'
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
+import Doctors from './components/Doctors/Doctors';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/doctores' element={<Doctors />}/>
       </Routes>
       <Footer />
     </HashRouter>
---
+++ src/components/Doctors/Doctors.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+import doctor from '../../img/doctor.png'
+import './Doctors.css'
+
+const Doctors = () => {
+  const [section, setSection] = React.useState(true)
+
+  return (
+    <div className="Doctors">
+      <h1 className="Doctors-title">Nuestros Profesionales de la Salud</h1>
+      <div className="Doctors-categories">
+        <div className="Doctors-categories__category">
+          <div className="Doctors-categories__category-head">
+            <h2>Médicos Generales</h2>
+            {section ? (
+              <span onClick={() => setSection(!section)}>
+                <IoIosArrowDown />
+              </span>
+            ) : (
+              <span onClick={() => setSection(!section)}>
+                <IoIosArrowUp />
+              </span>
+            )}
+          </div>
+          {section && (
+            <div className="Doctors-categories__category-body">
+              <div className="Doctors-categories__category-body__doctor">
+                <img src={doctor} alt="Guillermo Pascal" />
+                <h3>Guilermo Pascal</h3>
+                <h4>Médico General</h4>
+                <p>Santa Marta, Colombia</p>
+              </div>
+              <div className="Doctors-categories__category-body__doctor">
+                <img src={doctor} alt="Guillermo Pascal" />
+                <h3>Guilermo Pascal</h3>
+                <h4>Médico General</h4>
+                <p>Santa Marta, Colombia</p>
+              </div>
+              <div className="Doctors-categories__category-body__doctor">
+                <img src={doctor} alt="Guillermo Pascal" />
+                <h3>Guilermo Pascal</h3>
+                <h4>Médico General</h4>
+                <p>Santa Marta, Colombia</p>
+              </div>
+              <div className="Doctors-categories__category-body__doctor">
+                <img src={doctor} alt="Guillermo Pascal" />
+                <h3>Guilermo Pascal</h3>
+                <h4>Médico General</h4>
+                <p>Santa Marta, Colombia</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Doctors;
