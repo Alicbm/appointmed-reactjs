bfe43b502804d6701ecd15fbe8a140796344b939
alicbm <abarandica1234@gmail.com>
2023-07-18 09:30:08 -0500

    schedule route ready with mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 import Doctors from './components/Doctors/Doctors';
 import AllSchedule from './components/Schedule/AllSchedule';
+import EachSchedule from './components/Schedule/EachSchedule';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home />}/>
         <Route path='/doctores' element={<Doctors />}/>
         <Route path='/schedule' element={<AllSchedule />}/>
+        <Route path='/each-schedule' element={<EachSchedule />}/>
       </Routes>
       <Footer />
     </HashRouter>
--- src/components/Schedule/EachSchedule.tsx
+++ src/components/Schedule/EachSchedule.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import doctor from "../../img/doctor.png";
+import './EachSchedule.css'
+import Schedule from "./Schedule";
 
 const EachSchedule = () => {
+  const [morning, setMorning] = React.useState(true)
+  const [afternoon, setAfternoon] = React.useState(true)
+
   return (
-    <div className='EachSchedule'>
+    <div className="EachSchedule">
+      <h1 className="EachSchedule-title">Horarios para Medicina General</h1>
+      <div className="EachSchedule-carousel">
+        <div className="EachSchedule-carousel__doctor">
+          <img src={doctor} alt="Guillermo Pascal" />
+          <h3>Guilermo Pascal</h3>
+          <h4>Médico General</h4>
+          <p>Santa Marta, Colombia</p>
+        </div>
+        <div className="EachSchedule-carousel__doctor">
+          <img src={doctor} alt="Guillermo Pascal" />
+          <h3>Guilermo Pascal</h3>
+          <h4>Médico General</h4>
+          <p>Santa Marta, Colombia</p>
+        </div>
+        <div className="EachSchedule-carousel__doctor">
+          <img src={doctor} alt="Guillermo Pascal" />
+          <h3>Guilermo Pascal</h3>
+          <h4>Médico General</h4>
+          <p>Santa Marta, Colombia</p>
+        </div>
+        <div className="EachSchedule-carousel__doctor">
+          <img src={doctor} alt="Guillermo Pascal" />
+          <h3>Guilermo Pascal</h3>
+          <h4>Médico General</h4>
+          <p>Santa Marta, Colombia</p>
+        </div>
+      </div>
+
+      <Schedule 
+        state={morning} 
+        setState={setMorning}
+        title='Horario de la Mañana'
+      />
+      <Schedule 
+        state={afternoon} 
+        setState={setAfternoon}
+        title='Horario de la Tarde'
+      />
 
     </div>
-  )
-}
+  );
+};
 
-export default EachSchedule
+export default EachSchedule;
